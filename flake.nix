4978691ece97b500b29b699931967d8fff2d01c8
rykugur <rollhax@gmail.com>
2025-07-25 00:56:11 -0500

    working on nixOS

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Bun flake.";
+  description = "Flake for SPTMM (SPTarkov Mod Manager).";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -11,21 +11,52 @@
       systems =
         [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
 
-      perSystem = { pkgs, ... }: {
-        devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.bun
-
-            # tauri
-            pkgs.cargo
-            pkgs.rustc
-            pkgs.rustup
-
-            pkgs.nodejs
+      perSystem = { pkgs, lib, ... }:
+        let
+          libraries = lib.optionals pkgs.stdenv.isLinux [
+            pkgs.webkitgtk_4_1
+            pkgs.gtk3
+            pkgs.cairo
+            pkgs.gdk-pixbuf
+            pkgs.glib
+            pkgs.dbus
+            pkgs.openssl_3_5
+            pkgs.librsvg
           ];
-        };
+        in {
+          devShells.default = pkgs.mkShell {
 
-        formatter = pkgs.nixfmt-rfc-style;
-      };
+            packages = [
+              pkgs.bun
+
+              # tauri
+              pkgs.cargo
+              pkgs.rustc
+              pkgs.rustup
+
+              pkgs.nodejs
+            ] ++ (lib.optionals pkgs.stdenv.isLinux [
+              pkgs.curl
+              pkgs.wget
+              pkgs.pkg-config
+              pkgs.dbus
+              pkgs.openssl_3
+              pkgs.glib
+              pkgs.gtk3
+              pkgs.libsoup_2_4
+              pkgs.webkitgtk_4_1
+              pkgs.librsvg
+            ]);
+
+            shellHook = lib.optionalString pkgs.stdenv.isLinux ''
+              export LD_LIBRARY_PATH=${
+                pkgs.lib.makeLibraryPath libraries
+              }:$LD_LIBRARY_PATH
+              export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+            '';
+          };
+
+          formatter = pkgs.nixfmt-rfc-style;
+        };
     };
 }
