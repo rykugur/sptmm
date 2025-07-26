959b86084b639063ac7d1458cf59123dd8b78471
rykugur <rollhax@gmail.com>
2025-07-26 14:18:42 -0500

    this is better

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,4 +1,6 @@
-export const isTauri = "__TAURI_INTERNALS__" in window || "__TAURI__" in window;
+import { isTauri as _isTauri } from "@tauri-apps/api/core";
+
+export const isTauri = _isTauri();
 export const isWeb = !isTauri;
 
 export const isMobile = navigator.maxTouchPoints > 0;
